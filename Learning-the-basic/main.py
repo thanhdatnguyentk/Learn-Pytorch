bb96068ba08150677275ebb52b077dba20e04bea
nguyen thanh dat <ngthanhdat1303@gmail.com>
2024-09-20 14:03:49 +0700

    Fashion MNIST

--- Learning-the-basic/main.py
+++ Learning-the-basic/main.py
@@ -3,6 +3,18 @@ from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import Dataset, DataLoader
+from sklearn.metrics import confusion_matrix
 
 # Download training data from open datasets.
 training_data = datasets.FashionMNIST(
@@ -18,7 +30,7 @@ test_data = datasets.FashionMNIST(
     transform=ToTensor(),
 )
 
-batch_size = 64
+batch_size = 100
 
 # Create data loaders.
 train_dataloader = DataLoader(training_data, batch_size=batch_size)
@@ -28,7 +40,25 @@ for X, y in test_dataloader:
     print(f"Shape of X [N, C, H, W]: {X.shape}")
     print(f"Shape of y: {y.shape} {y.dtype}")
     break
+def output_label(label):
+    output_mapping = {
+                 0: "T-shirt/Top",
+                 1: "Trouser",
+                 2: "Pullover",
+                 3: "Dress",
+                 4: "Coat", 
+                 5: "Sandal", 
+                 6: "Shirt",
+                 7: "Sneaker",
+                 8: "Bag",
+                 9: "Ankle Boot"
+                 }
+    input = (label.item() if type(label) == torch.Tensor else label)
+    return output_mapping[input]
+a = next(iter(train_dataloader))
+a[0].size()
 
+print(len(train_dataloader))
 # Get cpu, gpu or mps device for training.
 device = (
     "cuda"
@@ -39,70 +69,124 @@ device = (
 )
 print(f"Using {device} device")
 
-# Define model
-class NeuralNetwork(nn.Module):
+class FashionCNN(nn.Module):
+    
     def __init__(self):
-        super().__init__()
-        self.flatten = nn.Flatten()
-        self.linear_relu_stack = nn.Sequential(
-            nn.Linear(28*28, 512),
+        super(FashionCNN, self).__init__()
+        
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
             nn.ReLU(),
-            nn.Linear(512, 512),
-            nn.ReLU(),
-            nn.Linear(512, 10)
+            nn.MaxPool2d(kernel_size=2, stride=2)
         )
-
+        
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+        
+        self.fc1 = nn.Linear(in_features=64*6*6, out_features=600)
+        self.drop = nn.Dropout2d(0.25)
+        self.fc2 = nn.Linear(in_features=600, out_features=120)
+        self.fc3 = nn.Linear(in_features=120, out_features=10)
+        
     def forward(self, x):
-        x = self.flatten(x)
-        logits = self.linear_relu_stack(x)
-        return logits
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc1(out)
+        out = self.drop(out)
+        out = self.fc2(out)
+        out = self.fc3(out)
+        
+        return out
 
-model = NeuralNetwork().to(device)
+model = FashionCNN()
+model.to(device)
+
+error = nn.CrossEntropyLoss()
+
+learning_rate = 0.001
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 print(model)
 
-loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+num_epochs = 5
+count = 0
+# Lists for visualization of loss and accuracy 
+loss_list = []
+iteration_list = []
+accuracy_list = []
 
-def train(dataloader, model, loss_fn, optimizer):
-    size = len(dataloader.dataset)
-    model.train()
-    for batch, (X, y) in enumerate(dataloader):
-        X, y = X.to(device), y.to(device)
+# Lists for knowing classwise accuracy
+predictions_list = []
+labels_list = []
 
-        # Compute prediction error
-        pred = model(X)
-        loss = loss_fn(pred, y)
-
-        # Backpropagation
-        loss.backward()
-        optimizer.step()
+for epoch in range(num_epochs):
+    for images, labels in train_dataloader:
+        # Transfering images and labels to GPU if available
+        images, labels = images.to(device), labels.to(device)
+    
+        train = Variable(images.view(100, 1, 28, 28))
+        labels = Variable(labels)
+        
+        # Forward pass 
+        outputs = model(train)
+        loss = error(outputs, labels)
+        
+        # Initializing a gradient as 0 so there is no mixing of gradient among the batches
         optimizer.zero_grad()
+        
+        #Propagating the error backward
+        loss.backward()
+        
+        # Optimizing the parameters
+        optimizer.step()
+    
+        count += 1
+    
+    # Testing the model
+    
+        if not (count % 50):    # It's same as "if count % 50 == 0"
+            total = 0
+            correct = 0
+        
+            for images, labels in test_dataloader:
+                images, labels = images.to(device), labels.to(device)
+                labels_list.append(labels)
+            
+                test = Variable(images.view(100, 1, 28, 28))
+            
+                outputs = model(test)
+            
+                predictions = torch.max(outputs, 1)[1].to(device)
+                predictions_list.append(predictions)
+                correct += (predictions == labels).sum()
+            
+                total += len(labels)
+            
+            accuracy = correct * 100 / total
+            loss_list.append(loss.data)
+            iteration_list.append(count)
+            accuracy_list.append(accuracy)
+        
+        if not (count % 500):
+            print("Iteration: {}, Loss: {}, Accuracy: {}%".format(count, loss.data, accuracy))
 
-        if batch % 100 == 0:
-            loss, current = loss.item(), (batch + 1) * len(X)
-            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+# Visualization loss
+plt.plot(iteration_list,loss_list)
+plt.xlabel("Number of iteration")
+plt.ylabel("Loss")
+plt.title("CNN: Loss vs Number of iteration")
+plt.show()
 
-def test(dataloader, model, loss_fn):
-    size = len(dataloader.dataset)
-    num_batches = len(dataloader)
-    model.eval()
-    test_loss, correct = 0, 0
-    with torch.no_grad():
-        for X, y in dataloader:
-            X, y = X.to(device), y.to(device)
-            pred = model(X)
-            test_loss += loss_fn(pred, y).item()
-            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
-    test_loss /= num_batches
-    correct /= size
-    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+# Visualization accuracy
+plt.plot(iteration_list,accuracy_list,color = "red")
+plt.xlabel("Number of iteration")
+plt.ylabel("Accuracy")
+plt.title("CNN: Accuracy vs Number of iteration")
+plt.show()
 
-epochs = 5
-for t in range(epochs):
-    print(f"Epoch {t+1}\n-------------------------------")
-    train(train_dataloader, model, loss_fn, optimizer)
-    test(test_dataloader, model, loss_fn)
-print("Done!")
 
-torch.save(model.state_dict(), "model.pth")
-print("Saved PyTorch Model State to model.pth")
